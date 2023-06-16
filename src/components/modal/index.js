fae0cef8c7773239d0a135a6f337c0ca9f5ed320
Yujin Choi <cuj10594@yonsei.ac.kr>
2023-06-16 11:12:03 +0900

    [design] set NoticeModal for common component

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -3,5 +3,6 @@ export { default as ConfirmModal } from "./ConfirmModal";
 export { default as CopyPivot } from "./CopyPivot";
 export { default as DeleteModal } from "./DeleteModal";
 export { default as LoginModal } from "./LoginModal";
+export { default as NoticeModal } from "./NoticeModal";
 export { default as SingleModal } from "./SingleModal";
 export { default as UnvalidModal } from "./UnvalidModal";
