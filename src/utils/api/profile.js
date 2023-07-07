c215d78876ecb165fc293ec80c3cda1952c38c91
KimSungKwon <sungkwonn@naver.com>
2023-07-07 17:01:48 +0900

    [migrate] migrate api: get my profile deco info

--- src/utils/api/profile.js
+++ src/utils/api/profile.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 import { privateHeadersMultipart, handleTokenExpired } from "./base";
 
+// (나의)프로필 꾸미기 정보 조회
 export const getProfileDeco = async (userId) => {
   let returnValue = 0;
-  await axios.get(`/public/profile?user_id=${userId}`).then((data) => {
-    returnValue = data.data.resultData;
-  });
+  
+  await axios.get(`/user/profile/decorate/:${userId}`)
+  .then((res) => {
+    returnValue = res.data.data;
+  })
+  .catch((error) => {
+    console.log("getProfileDeco: ", error);
+  })
 
   return returnValue;
 };
