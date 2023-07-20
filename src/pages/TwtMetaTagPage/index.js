601ade713b2132bbf4b739cfaafbb4f382c17a87
KimSungKwon <sungkwonn@naver.com>
2023-07-20 16:53:21 +0900

    [feat] add twitter card meta tags

--- src/pages/TwtMetaTagPage/index.js
+++ src/pages/TwtMetaTagPage/index.js
@@ -1,26 +1,18 @@
+import TwitterCard from "components/metatags/TwitterCard";
 import { useEffect } from "react";
-import { Helmet } from "react-helmet-async";
-
 const TwtMetaTagPage = () => {
 
   useEffect(() => {
-    window.location.href = "/";
+    //window.location.href = "/";
   }, [])
 
   return (  
     <>
-      <Helmet>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:image:src"
-          content="https://github.com/ziptalk/3tree-frontend/assets/99077953/93e23ac0-5d5f-4f9e-91f3-3ba8f1980a98"
-        />
-        <meta name="twitter:title" content="3TREE" />
-        <meta
-          name="twitter:description"
-          content="소셜계정만으로 간편하게 송금하기"
-        />
-      </Helmet>
+      <TwitterCard
+        title={"3TREE."}
+        description={"소셜계정만으로 간편하게 송금하기."}
+        image={"https://i.ibb.co/RSPsgsm/Twitter-Receive.png"}
+      />
     </>
   );
 }
