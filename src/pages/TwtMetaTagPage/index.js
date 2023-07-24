12288b401457ea214c70e41c188c14794e0497f5
KimSungKwon <sungkwonn@naver.com>
2023-07-24 11:25:32 +0900

    [fix] set hydrate dom

--- src/pages/TwtMetaTagPage/index.js
+++ src/pages/TwtMetaTagPage/index.js
@@ -3,15 +3,15 @@ import { useEffect } from "react";
 const TwtMetaTagPage = () => {
 
   useEffect(() => {
-    //window.location.href = "/";
+    window.location.href = "/";
   }, [])
 
   return (  
     <>
       <TwitterCard
-        title={"3TREE."}
-        description={"소셜계정만으로 간편하게 송금하기."}
-        image={"https://i.ibb.co/RSPsgsm/Twitter-Receive.png?1234"}
+        title={"3TREE "}
+        description={"소셜계정만으로 간편하게 송금하기 "}
+        image={"https://i.ibb.co/RSPsgsm/Twitter-Receive.png?123"}
       />
     </>
   );
