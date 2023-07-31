360df92509ed2f74900fa85d1aad998dc0e37415
KimSungKwon <sungkwonn@naver.com>
2023-07-31 11:52:38 +0900

    [chore] twitter metatag configure

--- src/pages/TwtMetaTagReceivePage/index.js
+++ src/pages/TwtMetaTagReceivePage/index.js
@@ -15,7 +15,7 @@ const TwtMetaTagPage = () => {
       <TwitterCard
         title={"3TREE "}
         description={"소셜계정만으로 간편하게 송금하기 "}
-        image={"https://i.ibb.co/RSPsgsm/Twitter-Receive.png?123"}
+        image={"https://i.ibb.co/RSPsgsm/Twitter-Receive.png"}
       />
     </>
   );
