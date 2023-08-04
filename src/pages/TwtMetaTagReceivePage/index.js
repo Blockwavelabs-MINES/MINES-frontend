108405dc9743b2501b18aaf342b034efc0228400
KimSungKwon <sungkwonn@naver.com>
2023-08-04 17:11:09 +0900

    [chore] set twitter meta tags (failed)

--- src/pages/TwtMetaTagReceivePage/index.js
+++ src/pages/TwtMetaTagReceivePage/index.js
@@ -13,8 +13,8 @@ const TwtMetaTagReceivePage = () => {
   return (  
     <>
       <TwitterCard
-        title={"3TREE."}
-        description={"소셜계정만으로 간편하게 송금하기."}
+        title={"3TREE"}
+        description={"소셜계정만으로 간편하게 송금하기"}
         image={"https://i.ibb.co/RSPsgsm/Twitter-Receive.png?124"}
       />
     </>
